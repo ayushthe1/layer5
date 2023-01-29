994a860c6e9af36cf9fec03242599ac4d5cfd28c
ayush_gitk <ayushsharmaa101@gmail.com>
2023-01-30 01:04:28 +0530

    update meta content=script
    Signed-off-by: ayush_gitk <ayushsharmaa101@gmail.com>

--- src/html.js
+++ src/html.js
@@ -7,7 +7,7 @@ export default function HTML(props) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta httpEquiv="Content-Security-Policy" content="default-src 'self'"></meta>
+        <meta httpEquiv="Content-Security-Policy" content="script-src 'self'"></meta>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
