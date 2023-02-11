d4233a3a630ec8ad92822ddc7a00468c891e91bf
ayush_gitk <ayushsharmaa101@gmail.com>
2023-02-11 20:00:42 +0530

    add server timeout and pattern
    Signed-off-by: ayush_gitk <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,6 +2,8 @@ module.exports = {
     "ci": {
       "collect": {
         "startServerCommand": "npx http-server -p 35341",
+        "startServerReadyPattern": "/listen|ready/i",
+        "startServerReadyTimeout": 2000,
         "url" : " http://localhost:35341/404.html" ,
       },
       "assert": {
