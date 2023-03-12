c3369cda79136c8287c309271a79f10a696351ea
ayush_gitk <ayushsharmaa101@gmail.com>
2023-03-12 15:02:22 +0530

    attempt1
    Signed-off-by: ayush_gitk <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,6 +3,14 @@ module.exports = {
       "collect": {
         "staticDistDir": "./public",
         "url" : ["http://localhost/404.html" ,"http://localhost/index.html","http://localhost/404/index.html","http://localhost/about/index.html"],
+        "settings": {
+          "skipAudits": [
+            {
+              "id": "aria-hidden-focus",
+              "pathPattern": "http://localhost/index.html",
+            }
+          ]
+        }
       },
       "assert": {
         "preset": "lighthouse:no-pwa",
