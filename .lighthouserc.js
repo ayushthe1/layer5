d708bcb7b156e0006b15149f837ef3e253c96863
ayush_gitk <ayushsharmaa101@gmail.com>
2023-03-12 17:00:05 +0530

    fix3
    Signed-off-by: ayush_gitk <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -5,8 +5,17 @@ module.exports = {
         "url" : ["http://localhost/404.html" ,"http://localhost/index.html","http://localhost/404/index.html","http://localhost/about/index.html"],
         "settings": [
           {
-            "skipAudits": ["aria-hidden-focus"],
+            "skipAudits": ["aria-hidden-label"],
             "url": "http://localhost/index.html"
+          },
+          {
+            "url": "http://localhost/404.html"
+          },
+          {
+            "url": "http://localhost/404/index.html"
+          },
+          {
+            "url": "http://localhost/about/index.html"
           }
         ]
       },
