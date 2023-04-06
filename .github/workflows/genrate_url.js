84251c4ee6803329fd0e0115889ba40d77157d6a
ayushthe1 <ayushsharmaa101@gmail.com>
2023-04-07 00:43:26 +0530

    fix 16
    Signed-off-by: ayushthe1 <ayushsharmaa101@gmail.com>

--- .github/workflows/genrate_url.js
+++ .github/workflows/genrate_url.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 // find all the HTML files in the ./public directory
 const htmlFiles = glob.sync('**/*.html', {
-    cwd: '../../public', 
+    cwd: './public', 
   });
   
   // generate the URL array based on the HTML files
