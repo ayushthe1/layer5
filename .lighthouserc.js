d8b2c1ca8e8a9e478ebea4e2a9fdda40ce83289c
ayushthe1 <ayushsharmaa101@gmail.com>
2023-04-06 19:06:48 +0530

    fix2
    Signed-off-by: ayushthe1 <ayushsharmaa101@gmail.com>

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -2,14 +2,7 @@ module.exports = {
     "ci": {
       "collect": {
         "staticDistDir": "./public",
-        "url" : ["http://localhost/404.html",
-                 "http://localhost/index.html",
-                 "http://localhost/404/index.html",
-                 "http://localhost/about/index.html" ,
-                 "http://localhost/cloud-native-management/meshery/index.html",
-                 "http://localhost/cloud-native-management/meshmap/index.html",
-                 "http://localhost/learn/index.html",
-                 "http://localhost/community/index.html"],
+        "url" : ["http://localhost/",]
       },
       "assert": {
         "preset": "lighthouse:no-pwa",
